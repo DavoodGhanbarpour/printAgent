85928234b05f63115cf89a605072893b7a0015ea
DavoodGhanbarpour <ghanbarpour.davood@gmail.com>
2022-08-27 22:53:20 +0430

    fixed sizing, added multi printer feature, better error handling

--- main.js
+++ main.js
@@ -1,44 +1,79 @@
-const { app, BrowserWindow,ipcMain,BrowserView } = require('electron');
+const { app, BrowserWindow,ipcMain,BrowserView, screen } = require('electron');
 const path        = require('path')
 const fs          = require("fs");
 const axios       = require('axios');
+const sleep     = require('sleep-promise');
 const CONFIG_PATH = './config/agentConfig.json';
 var win,view;        
-
-const createWindow = () => {
-  
-  win         = new BrowserWindow({
-    autoHideMenuBar: true,
-    width: 400,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-  win.loadFile('./src/index.html');
-  win.webContents.send('getConfig', getConfig())
-  // win.webContents.openDevTools()
-};
+app.disableHardwareAcceleration();
 
 app.whenReady().then(() => {
-  createWindow();
-  
-  ipcMain.on('setConfig', saveConfigs);
+  createAgentWindow();
   buildHiddenView();
-
+  ipcMain.on('setConfig', saveConfigs);
   setInterval(() => {
     getNextPrintJob();
   },2000);
 
   
-  app.on('activate', function () { if (BrowserWindow.getAllWindows().length === 0) createWindow() });
-})
+  app.on('activate', function () { 
+    if (BrowserWindow.getAllWindows().length === 0) 
+      createAgentWindow();
+  });
 
+  // win.on('minimize',function(event){
+  //   event.preventDefault();
+  //   win.minimize();
+  // });
+  
+  // win.on('close', function (event) {
+  //   if(!app.isQuiting){
+  //       event.preventDefault();
+  //       win.minimize();
+  //   }
+  //   return false;
+  // });
+  
+});
 
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
-})
+});
+
+
+
+
+
+const createAgentWindow = () => {
+  buildAgentMainWindow();
+  loadAgentHTML();
+  loadConfigToAgent();
+};
+
+function buildAgentMainWindow(){
+  let display = screen.getPrimaryDisplay();
+  win         = new BrowserWindow({
+    autoHideMenuBar: true,
+    width: 400,
+    height: 600,
+    x: display.bounds.width - 400,
+    y: display.bounds.height - 650,
+    maximizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  win.setResizable(false);
+}
+
+function loadConfigToAgent(){
+  win.webContents.send('getConfig', getConfig());
+}
+
+function loadAgentHTML(){
+  win.loadFile('./src/index.html');
+}
 
 function saveConfigs(event,input){
   fs.writeFile( CONFIG_PATH , JSON.stringify(input), function (err,data) {
@@ -56,24 +91,36 @@ function getConfig(){
     return "";
 }
 
+
 function getNextPrintJob(){
   let configs = getConfig();
   if( configs.ip && configs.port && configs.printerName )
   {
-    axios.get(`${configs.ip}:${configs.port}/system/prints/${configs.printerName}`)
-    .then(function (response) {
-      if( response.data != '-' )
-      {
-        preview(response.data+'&hide=true')
-        print(response.data+'&hide=true')
-      }
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .then(function () {
-    });  
+    configs.printerName.split(',').forEach(eachPrinterName => {
+      getPrinterJob( `${configs.ip}:${configs.port}`, eachPrinterName )
+    });
+    (async () => {
+      await sleep(1000);
+    })();
   }
+}
+
+
+function getPrinterJob( url, printerName )
+{
+  axios.get(`${url}/system/prints/${printerName}`)
+  .then(function (response) {
+    if( response.data != '-' )
+    {
+      preview(response.data+'&hide=true')
+      print(response.data+'&hide=true')
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+  });  
 }
 
 function preview(url){
@@ -86,18 +133,19 @@ function print(url){
   {
     console.log(url)
     view.webContents.loadURL(url)
-    view.webContents.print({
-      silent: true, 
-      printBackground: true, 
-      deviceName: configs.printerName
-    })
-
+    view.webContents.on('did-finish-load', function() {
+      view.webContents.print({
+        silent: true, 
+        printBackground: true, 
+        deviceName: configs.printerName
+      })  
+    });
   }
 }
 
 function buildHiddenView(){
   view = new BrowserView()
   win.setBrowserView(view)
-  view.setBounds({ x: 0, y: 0, width: 0, height: 0 })
+  view.setBounds({ x: 0, y: 550, width: 550, height: 550 })
 }
 
